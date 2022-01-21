27fa28b91778770c4e47347f12255d2007de434a
ngranzow <nate.granzow@gmail.com>
2022-01-21 17:32:20 -0600

    added custom fonts and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -286,4 +286,8 @@ $("#modalDueDate").datepicker({
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
